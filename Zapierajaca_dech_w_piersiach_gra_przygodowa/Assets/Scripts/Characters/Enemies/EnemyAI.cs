dcc8f12fb36ff86c3509e2f0066161e2447cae81
SmartMatt <43822422+SmartMaatt@users.noreply.github.com>
2021-04-21 00:22:21 +0200

    AI Zombie - 85%
    *Zombie posiada obszar ataku, słuchu oraz wzroku ograniczonego do 130 stopni (opcje skradankowe)
    *Zombie rozpoznaje zamknięte otoczenie co umożliwia gonić gracza w labiryntach
    *Zombie pamięta pozycję gracza gdy ten ukryje się za przeszkodą
    *W trakcie "szwędania się" Zombie wybiera losowe miejsce na podanym obszarze i podpoczywa pomiędzy trasami
    *Zombie rozpoznaje obiekty znajdujące się przed nim, przepaście oraz ściany
    *Naprawiono bład rozpoznowania gracza przez ścianę poprzez RayCasting
    *Naprawiono błąd wbiegania w przeszkodę gdy gracz jest poza zasięgiem (Zombie spokojnie czeka aż zejdzie)
    *Naprawiono błąd zbyt dużego łuku przy wchodzeniu w zakręt

--- Zapierajaca_dech_w_piersiach_gra_przygodowa/Assets/Scripts/Characters/Enemies/EnemyAI.cs
+++ Zapierajaca_dech_w_piersiach_gra_przygodowa/Assets/Scripts/Characters/Enemies/EnemyAI.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyAI : MonoBehaviour {
 
     private Animator _animator;
@@ -13,58 +15,76 @@ public class EnemyAI : MonoBehaviour {
         [SerializeField] private NavMeshAgent agent;
         [SerializeField] private Transform player;
         public LayerMask SolidGround, PlayerLayer;
- 
-  
+        public float obstacleRange = 0.5f;
+
     [Header("Patroling")]
         public float walkPointRange;
         public float patrolMoveSpeed;
+        public float hearRange; 
         public float sightRange;
-        Vector3 walkPoint;
-        bool walkPointSet, enemyReady, playerInSightRange;
+        [Range(10,180)]
+        public float sightConeRange;
+        public float maxRestTime;
+        public float minRestTime;
+        Vector3 _walkPoint;
+        bool walkPointSet, enemyReadyToPatrol, playerInHearRange, playerInSightRange;
 
     [Header("Attacking")]
         public float timeBetweenAttacks;
         public float attackMoveSpeed;
         public float attackRange;
-        bool alreadyAttacked, playerInAttackRange;
+        bool alreadyAttacked, playerInAttackRange, isChasing;
         //public GameObject projectile;
+
 
     private void Awake()
     {
-        //TO_DO: Correct player finding
-        player = GameObject.Find("Player").transform;
-        agent = GetComponent<NavMeshAgent>();
-
         _animator = GetComponent<Animator>();
         _currentMoveSpeed = patrolMoveSpeed;
+
+        player = FindObjectsOfType<RelativeMovement>()[0].transform;
+        agent = GetComponent<NavMeshAgent>();
+
+        _walkPoint = transform.position;
+        walkPointSet = false;
+        enemyReadyToPatrol = false;
+        playerInHearRange = false;
+        playerInSightRange = false;
+
+        alreadyAttacked = false;
+        playerInAttackRange = false;
+        isChasing = false;
     }
+
 
     private void Update()
     {
-        //Check for sight and attack range
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, PlayerLayer);
+        playerInSightRange = isPlayerSeen();
+        playerInHearRange = Physics.CheckSphere(transform.position, hearRange, PlayerLayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, PlayerLayer);
 
-        if (playerInAttackRange && playerInSightRange) { AttackPlayer(); }
-        else if (playerInSightRange && !playerInAttackRange) { ChasePlayer(); }
-        else if (!playerInSightRange && !playerInAttackRange) { Patroling(); }
+        if (playerInAttackRange) { AttackPlayer(); }
+        else if ((playerInHearRange && !playerInAttackRange) || (playerInSightRange)) { ChasePlayer(); }
+        else { Patroling(); }
 
-        _animator.SetFloat("Speed", _currentMoveSpeed);
-        agent.speed = _currentMoveSpeed;
-        Debug.Log("Current speed: " + _currentMoveSpeed + " ,Animation speed: " + _animator.GetFloat("Speed"));
+        checkWhatIsInFrontOf();
     }
+
 
     private void Patroling()
     {
-        if (!walkPointSet && !enemyReady) { SearchWalkPoint(); }
-        else if(walkPointSet && enemyReady) { agent.SetDestination(walkPoint); }
+        if (!walkPointSet && !enemyReadyToPatrol) { SearchWalkPoint(); }
+        else if(walkPointSet && enemyReadyToPatrol) { agent.SetDestination(_walkPoint); }
 
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        Vector3 distanceToWalkPoint = transform.position - _walkPoint;
 
-        //Walkpoint reached
-        if (distanceToWalkPoint.magnitude < 1f) { walkPointSet = false; enemyReady = false; }
+        if (distanceToWalkPoint.magnitude < 1f) {
+            walkPointSet = false; enemyReadyToPatrol = false;
+        }
         _animator.SetBool("Attack", false);
+        isChasing = false;
     }
+
 
     private void SearchWalkPoint()
     {
@@ -72,29 +92,33 @@ public class EnemyAI : MonoBehaviour {
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        _walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
 
-
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, SolidGround)) {
+        if (Physics.Raycast(_walkPoint, -transform.up, 2f, SolidGround)) {
             walkPointSet = true;
             _currentMoveSpeed = 0.0f;
             StartCoroutine(enemyWalkingPause());
         } 
     }
 
+
     private void ChasePlayer()
     {
-        agent.SetDestination(player.position);
-        transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
+        _walkPoint = player.position;
+        agent.SetDestination(_walkPoint);
+
+        enemyReadyToPatrol = false;
+        isChasing = true;
         _currentMoveSpeed = attackMoveSpeed;
         _animator.SetBool("Attack", false);
     }
 
+
     private void AttackPlayer()
     {
-        //Make sure enemy doesn't move
-        //agent.SetDestination(player.position);
-        transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
+        //transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
+        _animator.SetBool("Attack", true);
+        isChasing = true;
 
         if (!alreadyAttacked)
         {
@@ -108,16 +132,16 @@ public class EnemyAI : MonoBehaviour {
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
             */
-
-            Debug.Log("Attack!");
-            _animator.SetBool("Attack", true);
         }
     }
 
+
+    /*
     private void ResetAttack()
     {
         alreadyAttacked = false;
     }
+
 
     public void TakeDamage(int damage)
     {
@@ -125,26 +149,103 @@ public class EnemyAI : MonoBehaviour {
 
         if (_health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
     }
+    */
+
 
     private void DestroyEnemy()
     {
         Destroy(gameObject);
     }
 
+
+    private void checkWhatIsInFrontOf()
+    {
+        Vector3 lookrotation = agent.steeringTarget - transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookrotation), 20f * Time.deltaTime);
+
+        if (isChasing)
+        {
+            Ray ray = new Ray(new Vector3(transform.position.x, transform.position.y+1.5f, transform.position.z), transform.forward);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.SphereCast(ray, obstacleRange/2, out hit))
+            {
+                GameObject hitObject = hit.transform.gameObject;
+                if (hit.distance < obstacleRange)
+                {
+                    if (Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(player.position.x, player.position.z)) < obstacleRange * 4)
+                    {
+                        _currentMoveSpeed = 0.0f;
+                        transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
+                    }
+                }
+                else if(hitObject.GetComponent<RelativeMovement>() != null)
+                { 
+                    transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
+                }
+            }
+        }
+        _animator.SetFloat("Speed", _currentMoveSpeed);
+        agent.speed = _currentMoveSpeed;
+    }
+
+    private bool isPlayerSeen()
+    {
+        //[1] Check sight circle
+        if (Physics.CheckSphere(transform.position, sightRange, PlayerLayer))
+        {
+            Vector3 playerToMonster = player.position - transform.position;
+            Vector3 playerToMonsterSight = new Vector3(player.position.x, player.position.y + 1.5f, player.position.z) - new Vector3(transform.position.x, transform.position.y+1.5f, transform.position.z);
+            float cosOfMaxSightAngle = Mathf.Cos(Mathf.Deg2Rad * sightConeRange / 2);
+            float cosOfCurrentAngle = Vector3.Dot(playerToMonster, transform.forward) / (playerToMonster.magnitude * transform.forward.magnitude);
+
+            //[2] Check sight angle
+            if (cosOfCurrentAngle > cosOfMaxSightAngle)
+            {
+                Ray ray = new Ray(transform.position, playerToMonsterSight);
+                RaycastHit hit;
+
+                //[3] Check for obsticles with RayCast
+                if (Physics.SphereCast(ray, 0.75f, out hit))
+                {
+                    GameObject hitObject = hit.transform.gameObject;
+                    if (hitObject.GetComponent<RelativeMovement>() != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, hearRange);
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, sightRange);
-    }
+
+        Gizmos.color = Color.blue;
+        Ray ray = new Ray(new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z), transform.forward);
+        RaycastHit hit;
+
+        if (Physics.SphereCast(ray, obstacleRange / 2, out hit))
+        {
+            Gizmos.DrawWireSphere(hit.transform.position, obstacleRange/2);
+        }
+
+        }
+
 
     private IEnumerator enemyWalkingPause()
     {
-        float timeDelay = Random.Range(1.0f, 2.0f);
-        Debug.Log(timeDelay);
+        float timeDelay = Random.Range(minRestTime, maxRestTime);
         yield return new WaitForSeconds(timeDelay);
         _currentMoveSpeed = patrolMoveSpeed;
-        enemyReady = true;
+        enemyReadyToPatrol = true;
     }
 }
