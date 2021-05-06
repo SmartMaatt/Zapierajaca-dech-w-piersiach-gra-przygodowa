234f6657a43f8c03efcfe4204d57e512b43b795e
MultiPrice <43818051+MultiPrice@users.noreply.github.com>
2021-05-06 22:51:33 +0200

    Nowy Wygląd Ekwipunku
    * nowy wygląd na ekwipunku
    * rozwijane listy i dodawanie miejsca
    * dynamiczne dodawanie miejsca na nowe itemy

---
+++ Zapierajaca_dech_w_piersiach_gra_przygodowa/Assets/Scripts/Inventory/UIOperator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIOperator : MonoBehaviour
+{
+    public GameObject InventoryPanel;
+    public GameObject InventoryMainTitle;
+
+    bool _activInvetory = false;
+
+    void Start()
+    {
+        InventoryPanel.SetActive(_activInvetory);
+        InventoryMainTitle.SetActive(_activInvetory);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            _activInvetory = !_activInvetory;
+            InventoryPanel.SetActive(_activInvetory);
+            InventoryMainTitle.SetActive(_activInvetory);
+        }
+    }
+}
